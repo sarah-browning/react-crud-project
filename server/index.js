96cf7a75c94eed805cccfbb81330485e422aa25d
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-07 23:38:33 -0400

    Added Express server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+// const routes = require("./routes");
+const server = express();
+const port = 3001;
+
+server.use(cors());
+server.use(express.urlencoded({ extended: false }));
+server.use(express.json());
+
+//routes
+server.get('/', (req, res) => res.send("Hello!"));
+// server.use('/', routes);
+
+server.listen(port, () => console.log(`Express running on port: ${port}...`));
