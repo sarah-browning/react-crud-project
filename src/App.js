4353dfaf48f0436233514876cfb2d1b1800b80d1
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-08 02:39:58 -0400

    Add project specs and file structure for components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+/* 
+    For this project, you will create the CRUD functionality for an inventory system.
+    You will create the functionality to add, edit, and delete items and item categories.
+        For categories, you will just require the category name (and id which will be managed via an auto incrementing primary key).
+        For items, you will be required to store the category_id, title, description, price, quantity, and sku,
+    Your app should start by displaying a page of links (one to manage categories and one to manage items).
+    Use react router to load the content for each management area.
+ */
 function App() {
   return (
     <div className="App">
