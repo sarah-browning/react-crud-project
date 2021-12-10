b6db3bd973ba89f1b2f6d8ea640aaab231f26c6d
Sarah Browning <browning.sarah.a@gmail.com>
2021-12-10 18:27:24 -0400

    Change index.js to show Main.js; Add Main.js skeleton

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Main.css';
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      entries: [],
-      editingItem: null
-    };
-  }
-
-  // RENDER APP
   render() {
-    return (
-      <div className='Main'>
-          <a href="/">Home</a>
-          <br />
-          <a href="/items">Items</a>
-          <br />
-          <a href='/categories'>Categories</a>
-          <br />
+    return(
+      <div>
+        <h1>React CRUD Application</h1>
+        <ul className='header'>
+          <li><a href='/'>Home</a></li>
+          <li><a href='/items'>Items</a></li>
+          <li><a href='/categories'>Categories</a></li>
+        </ul>
+        <div className='content'>
+
+        </div>
       </div>
-    );
+    )
+    
   }
 }
 
