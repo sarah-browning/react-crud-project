67f744b5414bde44a7d318496442f58d1767a3e9
sarah-browning <browning.sarah.a@gmail.com>
2021-12-10 14:47:37 -0400

    Add Item EditForm with Edit and Delete functions

--- src/components/items/EditForm.js
+++ src/components/items/EditForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Button from './Button';
-import './Form.css';
+import '../css/Form.css';
 
 //  Initializes constructor and sets object state
 class EditForm extends Component {
@@ -8,20 +8,27 @@ class EditForm extends Component {
         super(props);
         this.state = {
             ...props.editing
-         };
+        }
     }
 
-    // Updates entry in and then clears values
+    // Updates entry in state array and then clears values
     _update = () => {
+        console.log('_update fired');
+        // console.log('_update ' + JSON.stringify(this.state));
         this.props.onUpdateEntry(this.state);
-        this._clear();        
+        this._clear();
     }
 
     // Clears and resets stored values
     _clear = () => {
         console.log('_clear fired');
         this.setState({
-            title: '' });
+            category_id: '',
+            title: '',
+            description: '',
+            price: '',
+            quantity: '',
+            sku: '' });
     }
 
     // Detects when a field changes and store the value
@@ -32,16 +39,41 @@ class EditForm extends Component {
         });
     }
 
-    // Displays the AddForm
+    // Displays the EditForm
     render() { 
         return ( 
             <div>
+                <label>Category ID</label>
+                <input type="text" placeholder="Enter Category ID" value={this.state.category_id} onChange={
+                    event => this._handleTextChange('category_id', event.target.value)
+                } />
+                <br />
                 <label>Title</label>
                 <input type="text" placeholder="Enter Title" value={this.state.title} onChange={
                     event => this._handleTextChange('title', event.target.value)
                 } />
                 <br />
-                 <Button title="Edit Category"/>
+                <label>Description</label>
+                <input type="text" placeholder="Enter Description" value={this.state.description} onChange={
+                    event => this._handleTextChange('description', event.target.value)
+                } />
+                <br />
+                <label>Price ($)</label>
+                <input type="text" placeholder="Enter Price ($)" value={this.state.price} onChange={
+                    event => this._handleTextChange('price', event.target.value)
+                }/>
+                <br />
+                <label>Quantity</label>
+                <input type="text" placeholder="Enter Quantity" value={this.state.quantity} onChange={
+                    event => this._handleTextChange('quantity', event.target.value)
+                }/>
+                <br />
+                <label>SKU</label>
+                <input type="text" placeholder="Enter SKU" value={this.state.sku} onChange={
+                    event => this._handleTextChange('sku', event.target.value)
+                 }/>
+                 <br />
+                 <Button title="Update Item" onClick={ this._update } />
             </div>
         );
     }
